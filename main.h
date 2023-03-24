a1cbe4d153587f410624addc143faaa60d9e7dc4
Anita Ngeri <122820440+anitangeri@users.noreply.github.com>
2023-03-24 09:27:53 +0100

    Update main.h

--- main.h
+++ main.h
@@ -33,12 +33,12 @@ struct fmt
 
 
 /**
- * typedef struct fmt fmt_t - Struct op
+ * typedef struct fmt fnt - Struct op
  *
  * @fmt: The format.
- * @fm_t: The function associated.
+ * @fnt: The function associated.
  */
-typedef struct fmt fmt_t;
+typedef struct fmt fnt;
 
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
@@ -75,11 +75,11 @@ char buffer[], int flags, char flag_ch, int width, int precision, int size);
 int print_non_printable(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
-/* Funcion to print memory address */
+/* Function to print memory address */
 int print_pointer(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
-/* Funciotns to handle other specifiers */
+/* Functions to handle other specifiers */
 int get_flags(const char *format, int *i);
 int get_width(const char *format, int *i, va_list list);
 int get_precision(const char *format, int *i, va_list list);
